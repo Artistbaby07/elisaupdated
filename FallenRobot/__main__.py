0e98ed4991b3f806eed407b4f94fc20b6746d86c
Artistbaby07 <130132729+Artistbaby07@users.noreply.github.com>
2024-01-21 08:56:44 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -707,9 +707,8 @@ def main():
 
     dispatcher.add_error_handler(error_callback)
 
-    else:
-        LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4, clean=True)
+    LOGGER.info("Using long polling.")
+    updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()
